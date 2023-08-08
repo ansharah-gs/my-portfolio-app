f20aeccc949c749c60af9c7535e8000e47453172
ansharahmir <ansharah.gs@gmail.com>
2023-08-07 22:41:20 -0600

    Updated Education Info

--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -1,14 +1,12 @@
 import React from "react";
 export default function EducationCard() {
-
   return (
     <>
       <div className="col-md">
         <div className="card">
           <div className="card-body">
             <h2 className="card-title">Education</h2>
-            
-            
+
             <ul className="list-unstyled">
               <li>
                 <h6 className="fw-bold">
@@ -16,17 +14,28 @@ export default function EducationCard() {
                   Islamabad, Pakistan 2015-2019
                 </h6>
                 <p>
-                  Sed et ornare quam. Cras scelerisque ex ultricies neque
-                  sodales tristique. Phasellus mattis dui ut enim tincidunt
-                  auctor. Maecenas euismod, orci eget varius elementum, nunc
-                  massa dignissim sapien, ac sagittis libero dolor vitae elit.
+                My Bachelor's in Computer Science with a Software Development specialization from FUUAST, Islamabad (2015-2019), was pivotal. I gained expertise in programming, algorithms, and software engineering, supported by practical projects that honed my skills. Collaborative projects improved teamwork and communication, while interactions with experienced faculty fueled a passion for learning in the tech field. This foundation continues to guide my journey in the fast-paced world of technology.
                 </p>
               </li>
               <li>
-                <h6 className="fw-bold">Islamabad College for Girls F-6/2 Islamabad /High School 2012-2014</h6>
+                <h6 className="fw-bold">
+                  Islamabad College for Girls F-6/2 Islamabad /High School
+                  2012-2014
+                </h6>
                 <p>
-                  Aenean nisl enim, dictum in odio ut, suscipit efficitur diam.
-                  Nam nec velit a odio porta efficitur nec at magna.
+                  My high school years, spanning from 2015 to 2019, were
+                  instrumental in shaping my academic foundation and personal
+                  development. The school's diverse curriculum, encompassing
+                  subjects from mathematics to the arts, equipped me with a
+                  well-rounded skill set. Beyond textbooks, interactive projects
+                  and collaborative activities fostered critical thinking and
+                  effective communication. Engaging with dedicated educators and
+                  peers, I cultivated a passion for learning and the drive to
+                  excel. This phase not only paved the way for my subsequent
+                  academic endeavors but also instilled values of teamwork and
+                  creativity. My high school education at ICG
+                  remains a cornerstone of my journey, inspiring a commitment to
+                  continuous growth and learning.
                 </p>
               </li>
             </ul>
